c1a2e49a2d501c18167752380d087851c0218f92
FarruxBoy <botirovfarrux21@gmail.com>
2024-03-03 20:42:44 +0500

    project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const typed = new Typed('#element', {
+    strings: ['Lasting memories','.......'],
+    typeSpeed: 70,
+    startDelay: 700,
+    backSpeed: 70,
+    backDelay: 1700,
+    loop: true,
+    loopCount : Infinity,   
+})
